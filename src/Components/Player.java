009bdfbc1b9c96539e025654a75344fbdc3e5a3a
aleksanderlj <aleksanderlj@hotmail.com>
2018-11-25 20:59:00 +0100

    Player og Account umiddelbart lavet //Aleksander

--- src/Components/Player.java
+++ src/Components/Player.java
@@ -6,6 +6,7 @@ public class Player {
     private String name;
     private Account account;
     private Property[] ownedProperties = new Property[16];
+    private int position = 0;
 
     public Player(String name, int startBalance){
         this.name = name;
@@ -32,23 +33,35 @@ public class Player {
             else
                 nextIndex = 0;
         }while(!propertyAdded);
-
-
     }
 
     public void sellProperty(Property sellProperty){
-        for(int n=0 ; n == ownedProperties.length-1 "Mangler at ramme sidste index???" ; n++){
-            if (ownedProperties[n] == sellProperty)
+        for(int n=0 ; n < ownedProperties.length; n++){
+            if (ownedProperties[n] == sellProperty){
                 addMoney(sellProperty.getValue());
+                sellProperty.setOwner(null);
                 ownedProperties[n] = null;
-                n = ownedProperties.length-1;
+                n = ownedProperties.length; //For at komme hurtigt ud af loop
+            }
         }
     }
 
     public void removeAllProperties(){
-        for(int n=0 ; n == ownedProperties.length-1 "Mangler at ramme sidste index???"; n++){
+        for(int n=0 ; n < ownedProperties.length ; n++){
+            ownedProperties[n].setOwner(null);
             ownedProperties[n] = null;
         }
+    }
+
+    //--------------------
+    // Getters og setters
+    //--------------------
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getBalance(){
@@ -57,5 +70,13 @@ public class Player {
 
     public void setBalance(int newBalance){
         account.setBalance(newBalance);
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
     }
 }
