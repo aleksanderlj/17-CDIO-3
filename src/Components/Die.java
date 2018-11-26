259b6437a2462e21cab84aca5d6318c491776abf
Jonatan <s185096@student.dtu.dk>
2018-11-26 11:45:33 +0100

    Importeret Die klasse, fra tidligere projekter \\Jonatan

--- src/Components/Die.java
+++ src/Components/Die.java
@@ -1,4 +1,32 @@
 package Components;
 
+import java.util.Random;
+
 public class Die {
+    private int faceValue;
+    private int faceAmount;
+
+    //---------------------------
+    //Constructor for Die klassen
+    //---------------------------
+    public Die(int faceAmount){
+        this.faceAmount = faceAmount;
+    }
+
+    //-----------------------------------
+    //Ruller terning og returner resultatet
+    //-----------------------------------
+    public int rul() {
+        Random r = new Random();
+        this.faceValue = r.nextInt(this.faceAmount) + 1;
+        return this.faceValue;
+    }
+
+    //-------------------
+    //Getters og setters
+    //-------------------
+    public int getFaceValue(){
+        return this.faceValue;
+    }
+
 }
