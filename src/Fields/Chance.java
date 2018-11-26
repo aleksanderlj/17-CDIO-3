b8c318e62297a85bfc55b6eac1da034d79352dc2
aleksanderlj <aleksanderlj@hotmail.com>
2018-11-26 14:41:18 +0100

    Chance-kort næsten lavet //Aleksander

--- src/Fields/Chance.java
+++ src/Fields/Chance.java
@@ -1,71 +1,191 @@
 package Fields;
 
-import Components.Player;
+import Components.*;
 
 public class Chance extends Field{
-    final int CARDAMOUNT = 24;
+    private static ChanceCards chanceCards = new ChanceCards();
+    private static int[] cardDeck = chanceCards.getCardDeck();
+    private static int cardIndex = 0;
+    private static Field[] fields = Board.getFields();
+    private static Player[] players = Board.getPlayers();
 
     //0=Kat, 1=Hund, 2=Bil, 3=Skib
     public void landOn(Player player) {
-        //Random til hvor du lander: 0-23 (hvis liste), 1-24 (hvis switch eller if-loop)
+        boolean cardTaken = false;
 
+        do {
+            if (cardIndex < cardDeck.length) {
+                switch (cardDeck[cardIndex]) {
+                    case 1: {
+
+                        break;
+                    }
+                    case 2: {
+                        player.setPosition(0);
+                        fields[0].landOn(player);
+                        break;
+                    }
+                    case 3: {
+                        int choice = ???1, 2, 3, 4, 5;
+                        player.move(choice);
+                        fields[player.getPosition()].landOn(player);
+                        break;
+                    }
+                    case 4: {
+                        int choice = ???10, 11;
+                        player.setPosition(choice);
+                        if (fields[choice].hasOwner())
+                            fields[choice].payRent(player);
+                        else
+                            player.addProperty(fields[choice]);
+                        break;
+                    }
+                    case 5: {
+                        int choice = ???1, 2;
+                        if (choice == 1) {
+                            player.move(1);
+                            fields[player.getPosition()].landOn(player);
+                        } else
+                            landOn(player);
+                        break;
+                    }
+                    case 6: {
+
+                        break;
+                    }
+                    case 7: {
+                        player.addMoney(-2);
+                        break;
+                    }
+                    case 8: {
+                        int choice = ???10, 11, 19, 20;
+                        player.setPosition(choice);
+                        if (fields[choice].hasOwner())
+                            fields[choice].payRent(player);
+                        else
+                            player.addProperty(fields[choice]);
+                        break;
+                    }
+                    case 9: {
+                        int choice = 4, 5;
+                        player.setPosition(choice);
+                        if (fields[choice].hasOwner())
+                            fields[choice].payRent(player);
+                        else
+                            player.addProperty(fields[choice]);
+                        break;
+                    }
+                    case 10: {
+                        player.setReleaseCard(true);
+                        break;
+                    }
+                    case 11: {
+                        player.setPosition(23);
+                        fields[23].landOn();
+                        break;
+                    }
+                    case 12: {
+
+                        break;
+                    }
+                    case 13: {
+
+                        break;
+                    }
+                    case 14: {
+                        for (int n = 0; n < players[].length; n++) {
+                            players[n].addMoney(-1);
+                            player.addMoney(1);
+                        }
+                        break;
+                    }
+                    case 15: {
+                        int choice = 7, 8, 22, 23;
+                        player.setPosition(choice);
+                        if (fields[choice].hasOwner())
+                            fields[choice].payRent(player);
+                        else
+                            player.addProperty(fields[choice]);
+                        break;
+                    }
+                    case 16: {
+                        player.addMoney(2);
+                        break;
+                    }
+                    case 17: {
+                        int choice = 13, 14;
+                        player.setPosition(choice);
+                        if (fields[choice].hasOwner())
+                            fields[choice].payRent(player);
+                        else
+                            player.addProperty(fields[choice]);
+                        break;
+                    }
+                    case 18: {
+                        player.setPosition(10);
+                        if (fields[10].hasOwner())
+                            fields[10].payRent(player);
+                        else
+                            player.addProperty(fields[10]);
+                        break;
+                    }
+                    case 19: {
+
+                        break;
+                    }
+                    case 20: {
+
+                        break;
+                    }
+                    case 21: {
+
+                        break;
+                    }
+                    case 22: {
+
+                        break;
+                    }
+                    case 23: {
+                        int choice = 4, 5, 13, 14;
+                        player.setPosition(choice);
+                        if (fields[choice].hasOwner())
+                            fields[choice].payRent(player);
+                        else
+                            player.addProperty(fields[choice]);
+                        break;
+                    }
+                    case 24: {
+                        int choice = 1, 2, 16, 17;
+                        player.setPosition(choice);
+                        if (fields[choice].hasOwner())
+                            fields[choice].payRent(player);
+                        else
+                            player.addProperty(fields[choice]);
+                        break;
+                    }
+                    default: {
+
+                        break;
+                    }
+                }
+                cardIndex++;
+                cardTaken = true;
+            } else {
+                cardDeck = chanceCards.shuffle(cardDeck);
+                cardIndex = 0;
+                cardTaken = false;
+            }
+        } while(!cardTaken);
+
+
+        //***********"LAV AMOUNT OF CARDS MINDRE HVIS DU IKKE INDSÆTTER SPILLER-SPECIFIKKE CHANCEKORT";************
+        /*
+        // Spiller-specifikke chancekort:
         //1
 
 
-        //2
-        player.setPosition(0);
-        fields[0].landOn(player);
-
-        //3
-        int choice = ??? 1,2,3,4,5;
-        player.move(choice);
-        fields[player.getPosition()].landOn(player);
-
-        //4
-        int choice = ??? 10,11;
-        player.setPosition(choice);
-        if (field[choice].hasOwner())
-            field[choice].payRent(player);
-        else
-            player.addProperty(fields[choice]);
-
-        //5
-        int choice = ??? 1,2;
-        if (choice==1){
-            player.move(1);
-            fields[player.getPosition()].landOn(player);
-        }
-        else
-            landOn(player);
-
         //6
 
-
-        //7
-        player.addMoney(-2);
-
-        //8
-        int choice = ??? 10,11,19,20;
-        player.setPosition(choice);
-        if (field[choice].hasOwner())
-            field[choice].payRent(player);
-        else
-            player.addProperty(fields[choice]);
-
-        //9
-        int choice = 4,5;
-        player.setPosition(choice);
-        if (field[choice].hasOwner())
-            field[choice].payRent(player);
-        else
-            player.addProperty(fields[choice]);
-
-        //10
-        player.setReleaseCard(true);
-
-        //11
-        player.setPosition(23);
-        field[23].landOn();
 
         //12
 
@@ -73,39 +193,11 @@ public class Chance extends Field{
         //13
 
 
-        //14
-        for(int n=0 ; n < players[].length ; n++) {
-            players[n].addMoney(-1);
-            player.addMoney(1);
-        }
-
-        //15
-        int choice = 7,8,22,23;
-        player.setPosition(choice);
-        if (field[choice].hasOwner())
-            field[choice].payRent(player);
-        else
-            player.addProperty(fields[choice]);
-
-        //16
-        player.addMoney(2);
-
-        //17
-        int choice = 13,14;
-        player.setPosition(choice);
-        if (field[choice].hasOwner())
-            field[choice].payRent(player);
-        else
-            player.addProperty(fields[choice]);
-
-        //18
-
-
-        //19 LAV if-statement ALT EFTER OM DER ER EN FJERDE SPILLER
+        //19 LAV if-statement ALT EFTER OM DER ER EN FJERDE SPILLER. ELLERS SKAL DE TAGE ET NYT KORT.
         players[3].setPosition(0);
         fields[0].landOn(players[3]);
 
-        //20 LAV if-statement ALT EFTER OM DER ER EN TREDJE SPILLER
+        //20 LAV if-statement ALT EFTER OM DER ER EN TREDJE SPILLER. ELLERS SKAL DE TAGE ET NYT KORT.
         players[2].move(1);
         fields[players[2].getPosition()].landOn(players[2]);
 
@@ -116,23 +208,16 @@ public class Chance extends Field{
         // players[0].setPosition(forrest + 1);
         // fields[players[2].getPosition()].landOn(players[2]);
 
-
-        //23
-        int choice = 4,5,13,14;
-        player.setPosition(choice);
-        if (field[choice].hasOwner())
-            field[choice].payRent(player);
-        else
-            player.addProperty(fields[choice]);
-
-        //24
-        int choice = 1,2,16,17;
-        player.setPosition(choice);
-        if (field[choice].hasOwner())
-            field[choice].payRent(player);
-        else
-            player.addProperty(fields[choice]);
+        */
 
 
+    }
+
+    public static int getCardIndex() {
+        return cardIndex;
+    }
+
+    public static int[] getCardDeck() {
+        return cardDeck;
     }
 }
