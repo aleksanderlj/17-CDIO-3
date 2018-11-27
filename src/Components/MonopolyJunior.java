a6d18a2ba221fb34b4ca04c17f17531f5927e601
Andreas <mail.Tailspot@gmail.com>
2018-11-27 16:10:07 +0100

    Det meste af main metode er lavet //Andreas

--- src/Components/MonopolyJunior.java
+++ src/Components/MonopolyJunior.java
@@ -5,11 +5,56 @@ import Fields.Field;
 public class MonopolyJunior {
     private static Board board  = new Board();
 
+    static Player[] players = board.createPlayers();
+    //Gætter på der skal laves en createPlayers metode
+    static Field[] field = board.getFields();
+    static int[] ting = new int[players.length];
+
     public static void main(String[] args) {
-        Player[] players = board.createPlayers();
-        Field[] field = board.getFields();
+        board.createBoard();
 
+        Die die = new Die(6);
 
+        for (int n = 0;n < players.length;n++){
+            while(players[n].getBalance() < 0){
+
+                int roll = die.roll();
+
+                players[n].move(roll);
+
+                field[players[n].getPosition()].landOn(players[n]);
+
+                if(n == players.length - 1){
+                    n = 0;
+                }
+                if (players[n].getBalance() < 0){
+                    endGame();
+                }
+            }
+        }
+    }
+
+    public static void endGame(){
+        for (int n = 0;n < players.length;n++){
+            ting[n] = players[n].getBalance();
+        }
+        int max = getMax(ting);
+        for(int n = 0;n < players.length;n++){
+            if(max == players[n].getBalance()){
+                players[n].win;
+                //Lav en win metode
+            }
+        }
+    }
+
+    public static int getMax(int[] inputArray){
+        int maxValue = inputArray[0];
+        for(int n = 1;n < inputArray.length;n++){
+            if(inputArray[n] > maxValue){
+                maxValue = inputArray[n];
+            }
+        }
+        return maxValue;
     }
 
     public static Board getBoard(){
